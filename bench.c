a76c6b4428d733dbd9f7e781984060c1d3b9172f
PJ <pjchiou0418@gmail.com>
2018-10-16 20:02:49 +0800

    Change prefix[3] to Prefix[4]

--- bench.c
+++ bench.c
@@ -22,7 +22,7 @@ double tvgetf()
 
 int bench_test(const tst_node *root, char *out_file, const int max)
 {
-    char prefix[3] = "";
+    char prefix[4] = "";
     char word[WORDMAX] = "";
     char **sgl;
     FILE *fp = fopen(out_file, "w");
@@ -50,7 +50,7 @@ int bench_test(const tst_node *root, char *out_file, const int max)
         t1 = tvgetf();
         tst_search_prefix(root, prefix, sgl, &sidx, max);
         t2 = tvgetf();
-        fprintf(fp, "%d %f sec\n", idx, (t2 - t1) * 1000000);
+        fprintf(fp, "%d %s %f sec\n", idx, prefix, (t2 - t1) * 1000000);
         idx++;
     }
 
